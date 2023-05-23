ae4742d0b3a7b5d30502e13c781a425ed2d896b9
ramonsouza36 <ramonriuller@outlook.com>
2023-05-23 20:03:52 -0300

    Adiçao de funções no Service do Instructor

--- SchoolManagerSystem/Services/InstructorsService.cs
+++ SchoolManagerSystem/Services/InstructorsService.cs
@@ -13,5 +13,23 @@ namespace SchoolManagerSystem.Services
             Console.WriteLine($"{instructor!.FirstOrDefault()!.Name}");
             return instructor;
         }
+
+        public async Task SetInstructorAsync(IDbContextFactory<ApplicationDbContext> DbFactory, Instructor instructor)
+        {
+            using var context = DbFactory.CreateDbContext();
+            instructor.Id = Guid.NewGuid();
+            if(instructor is not null && instructor.Id != Guid.Empty)
+                context.Instructor.Add(instructor);
+            Console.WriteLine($"{instructor!.Name}");
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<Instructor> GetInstructorsByIdAsync(IDbContextFactory<ApplicationDbContext> DbFactory, Guid id)
+        {
+            using var context = DbFactory.CreateDbContext();
+            var instructor = context.Instructor.Where(i => i.Id != Guid.Empty).FirstOrDefault();
+            return instructor;
+
+        }
     }
 }
